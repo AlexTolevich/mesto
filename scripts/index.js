768569521476ab7a660e36760a0d36849cdbfb11
AlexanderKuznetsov <alexanderanatolevic@yandex.ru>
2021-08-22 22:40:09 +0500

    fix: work of the popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,23 @@
 let popup = document.querySelector('.popup');
-let formElement = document.querySelector('.popup__container');
+let editButton = document.querySelector('.profile__edit-button');
+let formElement = document.querySelector('.popup__form');
+let closePopupButton = document.querySelector('.popup__close-button');
 let nameInput = formElement.querySelector('.popup__input-name');
 let jobInput = formElement.querySelector('.popup__input-job');
-let editButton = document.querySelector('.profile__edit-button');
-let closePopupButton = document.querySelector('.popup__close-button');
 let profileName = document.querySelector('.profile__name')
 let profileJob = document.querySelector('.profile__job')
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
-    profileName.textContent=nameInput.value;
-    profileJob.textContent=jobInput.value;
+    profileName.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value;
     closePopup();
 }
 
 function openPopup() {
   popup.classList.add('popup_opened');
-  nameInput.value=profileName.textContent;
-  jobInput.value=profileJob.textContent;
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
 }
 
 function closePopup() {
