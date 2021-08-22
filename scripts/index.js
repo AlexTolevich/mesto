b36d02e9593fa73d84dff577682182994a1b2a81
AlexanderKuznetsov <alexanderanatolevic@yandex.ru>
2021-08-22 19:37:35 +0500

    feat: logic popup on JS

---
+++ scripts/index.js
@@ -0,0 +1,29 @@
+let popup = document.querySelector('.popup');
+let formElement = document.querySelector('.popup__container');
+let nameInput = formElement.querySelector('.popup__input-name');
+let jobInput = formElement.querySelector('.popup__input-job');
+let editButton = document.querySelector('.profile__edit-button');
+let closePopupButton = document.querySelector('.popup__close-button');
+let profileName = document.querySelector('.profile__name')
+let profileJob = document.querySelector('.profile__job')
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    profileName.textContent=nameInput.value;
+    profileJob.textContent=jobInput.value;
+    closePopup();
+}
+
+function openPopup() {
+  popup.classList.add('popup_opened');
+  nameInput.value=profileName.textContent;
+  jobInput.value=profileJob.textContent;
+}
+
+function closePopup() {
+  popup.classList.remove('popup_opened');
+}
+
+editButton.addEventListener('click', openPopup);
+closePopupButton.addEventListener('click', closePopup);
+formElement.addEventListener('submit', formSubmitHandler);
